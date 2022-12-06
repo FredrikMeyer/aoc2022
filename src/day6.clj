13eeeeca6c4812c81f796faedfd997111d00fa7a
Fredrik Meyer <hrmeyer@gmail.com>
2022-12-06 16:43:52 +0100

    Format buffer

--- src/day6.clj
+++ src/day6.clj
@@ -1,7 +1,6 @@
 (ns day6
   (:require
-   [clojure.java.io :as io]
-   [clojure.string :as str]))
+   [clojure.java.io :as io]))
 
 (def data-file "day6.txt")
 
@@ -12,16 +11,13 @@
 (def test "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg")
 
 (def real-data
-  (slurp (io/resource "day6.txt"))
-  )
+  (slurp (io/resource "day6.txt")))
 
 (defn find-first-occurence-of-difference [s n-distinct]
-  (loop [
-         ind 0]
+  (loop [ind 0]
     (let [current-window (subs s ind (+ ind n-distinct))]
       (if (= (count (set current-window)) n-distinct) (+ ind n-distinct)
-          (recur (inc ind))
-          ))))
+          (recur (inc ind))))))
 
 (defn q1 []
   (find-first-occurence-of-difference real-data 4))
